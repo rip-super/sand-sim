2e3fa3e1f244e7ae8b2985925e75a17ce1e2a1b5
rip_super <sahildash7704@gmail.com>
2026-04-23 23:46:23 -0500

    got window opening

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use rusty_console_game_engine::prelude::*;
+
+struct SandSim;
+
+impl ConsoleGame for SandSim {
+    fn app_name(&self) -> &str {
+        "Falling Sand Simulation"
+    }
+
+    fn create(&mut self, _engine: &mut ConsoleGameEngine<Self>) -> bool {
+        true
+    }
+
+    fn update(&mut self, engine: &mut ConsoleGameEngine<Self>, _elapsed_time: f32) -> bool {
+        engine.clear(FG_BLACK);
+
+        true
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut engine = ConsoleGameEngine::new(SandSim);
+    engine
+        .construct_console(150, 150, 4, 4)
+        .expect("Console Construction Failed");
+    engine.start();
 }
